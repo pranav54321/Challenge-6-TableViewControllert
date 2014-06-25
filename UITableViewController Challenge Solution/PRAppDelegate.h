f06b5e8af290152af74daadb514c478b530d53cb
Pranav Ranawat <pranavmahendra@pranavs-iMac.local>
2014-06-25 11:20:06 +0530

    Initial Commit

---
+++ UITableViewController Challenge Solution/PRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PRAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Pranav Ranawat on 25/06/14.
+//  Copyright (c) 2014 Pranav Ranawat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Pranav Ranawat on 25/06/14.
+//  Copyright (c) 2014 Pranav Ranawat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PRAppDelegate class]));
+    }
+}
